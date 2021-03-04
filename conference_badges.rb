aab7a72d984dfb4b1e4d54e87bd64aafeba77a5b
Milan <milanvpage@gmail.com>
2021-03-04 15:40:03 -0800

    commit tree 545c73958b4591a8950dc400daba25362babbfe8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+    
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    new_attendees = []
+    attendees.each do |attendee|
+        new_attendees << "Hello, my name is #{attendee}."
+
+        
+        "Hello, my name is #{attendee}."
+    end
+    new_attendees
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each_with_index do |attendee, index|
+        room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+
+
+        "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+    end
+    room_assignments
+end
+
+def printer(attendees)
+    #binding.pry
+    batch_badge_creator(attendees).each do |attendee|
+        puts attendee
+    end
+    assign_rooms(attendees).each do |attendee|
+        puts attendee
+    end
+end
+
+
